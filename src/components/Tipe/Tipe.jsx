3a3251cfb0fc35a5014f2ad88c12e3f577fe5ae4
David Arellano <dtarellano1@gmail.com>
2018-05-29 16:21:05 -0700

    fixed tipe logo component

--- src/components/Tipe/Tipe.jsx
+++ src/components/Tipe/Tipe.jsx
@@ -3,13 +3,15 @@ import Logo from '../../assets/tipeLogo.png';
 
 export default () => {
   return (
-    <img
-      className="sidebar-item"
-      src={ Logo }
-      style={{
-        width: '50%'
-      }}
-      href="https://tipe.io/?ref=webpack"
-    />
+    <a className="sidebar-item" href="https://tipe.io/?ref=webpack">
+      <img
+        src="https://cdn.tipe.io/tipe/tipe-ad.png"
+        alt="Tipe Logo"
+        style={{
+          width: '50%',
+          height: '50%'
+        }}
+      />
+    </a>
   );
 };
