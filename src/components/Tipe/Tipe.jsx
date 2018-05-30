0be151629465b27593a33f4e6de54f83b3258213
David Arellano <dtarellano1@gmail.com>
2018-05-29 17:12:15 -0700

    added width to cdn query

--- src/components/Tipe/Tipe.jsx
+++ src/components/Tipe/Tipe.jsx
@@ -4,9 +4,9 @@ export default () => {
   return (
     <a className="sidebar-item" href="https://tipe.io/?ref=webpack">
       <img
-        src="https://cdn.tipe.io/tipe/tipe-ad.png?h=50" srcSet="https://cdn.tipe.io/tipe/tipe-ad.png?h=50 1x,
-          https://cdn.tipe.io/tipe/tipe-ad.png?h=50&dpr=2 2x,
-          https://cdn.tipe.io/tipe/tipe-ad.png?h=50&dpr=3 3x"
+        src="https://cdn.tipe.io/tipe/tipe-ad.png?h=50&w=86" srcSet="https://cdn.tipe.io/tipe/tipe-ad.png?h=50&w=86 1x,
+          https://cdn.tipe.io/tipe/tipe-ad.png?h=50&w=86&dpr=2 2x,
+          https://cdn.tipe.io/tipe/tipe-ad.png?h=50&w=86&dpr=3 3x"
           alt="Tipe" width="86" height="50"
         />
     </a>
