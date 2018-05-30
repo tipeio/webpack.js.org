78a4d78ea7cf4a524a659590aa3faeab05bb9dc3
David Arellano <dtarellano1@gmail.com>
2018-05-29 17:03:22 -0700

    updated tipe component with srcset

--- src/components/Tipe/Tipe.jsx
+++ src/components/Tipe/Tipe.jsx
@@ -4,13 +4,11 @@ export default () => {
   return (
     <a className="sidebar-item" href="https://tipe.io/?ref=webpack">
       <img
-        src="https://cdn.tipe.io/tipe/tipe-ad.png"
-        alt="Tipe Logo"
-        style={{
-          width: '50%',
-          height: '50%'
-        }}
-      />
+        src="https://cdn.tipe.io/tipe/tipe-ad.png?h=50" srcSet="https://cdn.tipe.io/tipe/tipe-ad.png?h=50 1x,
+          https://cdn.tipe.io/tipe/tipe-ad.png?h=50&dpr=2 2x,
+          https://cdn.tipe.io/tipe/tipe-ad.png?h=50&dpr=3 3x"
+          alt="Tipe" width="86" height="50"
+        />
     </a>
   );
 };
